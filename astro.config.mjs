9408026e1057eb9a2748e65d55f45a0f04bfbd49
modecode22 <epiccoder00@gmail.com>
2023-09-14 21:39:33 +0100

    added the vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import { remarkReadingTime } from './src/utils/remark-reading-time.mjs';
 import mdx from "@astrojs/mdx";
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone"
-  }),
+  adapter: vercel(),
   markdown: {
     remarkPlugins: [remarkReadingTime]
   },
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx()]
 });
