b537ff7ec66d2cc3941e0a2bf6785ae31231af83
modecode22 <epiccoder00@gmail.com>
2023-09-15 16:52:22 +0100

    merged the utils of the lib folder with utils of utils folder

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,37 @@
 import { parse, formatDistanceToNow } from "date-fns";
+import * as nodemailer from "nodemailer";
+const MY_EMAIL = import.meta.env.MY_EMAIL;
+const MY_PASSWORD = import.meta.env.MY_PASSWORD;
+
+export const sendEmail = async ({
+  name,
+  email,
+  message,
+}: {
+  name: string;
+  email: string;
+  message: string;
+}): Promise<void> => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: MY_EMAIL,
+      pass: MY_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: MY_EMAIL,
+    to: "aissaouimoncefdev@gmail.com",
+    subject: `Contact form submission from ${name}`,
+    text: `Email: ${email}\nMessage: ${message}`,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
 
 
 export const howManyDaysAgo = (date: string): string => {
